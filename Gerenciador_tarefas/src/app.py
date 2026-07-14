002cf8d005748bd27a828f942ddec7e427a79108
stefany_hagne <santana.stefany.iar@gmail.com>
2026-07-13 23:51:06 -0300

    Conectando código ao banco de dados

---
+++ Gerenciador_tarefas/src/app.py
@@ -0,0 +1,9 @@
+''' 
+PROJETO CRUD PARA GERENCIAMENTO DE TAREFAS
+Estudante: Stefany Hagne Oliveira Santana
+Matéria: Software Engineer
+Universidade: Centro Universitário UNIFECAF
+'''
+## Importações
+
+from conectar_db import conectar
---
+++ Gerenciador_tarefas/src/conectar_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+from pathlib import Path
+
+def conectar():
+    caminho_banco = (
+        Path(__file__).parent.parent
+        / "db"
+        / "gerencia_tarefas_bancodedados.db"
+    )
+
+    return sqlite3.connect(caminho_banco)
+
+conexao = conectar()
+print("Banco conectado com sucesso!")
+conexao.close()
+print("Conexão encerrada.")
